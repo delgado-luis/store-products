8b3cf737de17b9e78f94dd6051bc940d43ee64eb
Luis Gonsalo Delgado Zapata <delgadozapataluisgonsalo1@gmail.com>
2026-04-05 23:19:43 -0500

    configure environment variables

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
+import { envs } from './config/envs.ts';
 import { AppRoutes } from './routes/app.routes.ts';
 import { Server } from './server.ts';
 
 function main() {
   const appRoutes = new AppRoutes();
 
-  const server = new Server(3000, appRoutes.router());
+  const server = new Server(envs.PORT, appRoutes.router());
 
   server.start();
 }
